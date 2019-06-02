77bad3d9836a1a75f240ff252bcb8907abf7f153
Bruno Eiji <brunoeijioy@gmail.com>
2019-06-01 23:54:08 -0300

    avoid obstacle fuzzy working on mac

--- vrep-robot-master/examples/avoid_obstacles_fuzzy.py
+++ vrep-robot-master/examples/avoid_obstacles_fuzzy.py
@@ -2,42 +2,64 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
+import math
+import time
+import sys
+sys.path.insert(0, '../src')
+from robot import Robot
+
+
 class avoid_obstacles():
-	def __init__(self):
-		self.NUM_SENSORS = 8
-		self.max_speed = math.pi
-		self.steps = 0.01
+    def __init__(self):
+        self.NUM_SENSORS = 8
+        self.max_speed = math.pi
+        self.steps = 0.01
 
-	def init_fuzzy(self):
-		distance=[]
-		for i in range(0, self.NUM_SENSORS):
-			distance.append(ctrl.Antecedent(np.arange(0, 5.0, self.steps), 's'+str(i)))
-			distance[i]['close'] = fuzz.trapmf(distance[i].universe, [0.0, 0.0, 0.3, 0.6])
-			distance[i]['away'] = fuzz.trapmf(distance[i].universe, [0.3, 0.6, 5.0, 5.0])
+    def init_fuzzy(self):
+        distance = []
+        for i in range(0, self.NUM_SENSORS):
+            distance.append(ctrl.Antecedent(np.arange(0, 5.0, self.steps), 's' + str(i)))
+            distance[i]['close'] = fuzz.trapmf(distance[i].universe, [0.0, 0.0, 0.3, 0.6])
+            distance[i]['away'] = fuzz.trapmf(distance[i].universe, [0.3, 0.6, 5.0, 5.0])
 
-		v_left = ctrl.Consequent(np.arange(-self.max_speed, 1.0+self.max_speed, 0.01), 'vl')
-		v_right = ctrl.Consequent(np.arange(-self.max_speed, 1.0+self.max_speed, 0.01), 'vr')
+        v_left = ctrl.Consequent(np.arange(-self.max_speed, 1.0 + self.max_speed, 0.01), 'vl')
+        v_right = ctrl.Consequent(np.arange(-self.max_speed, 1.0 + self.max_speed, 0.01), 'vr')
 
-		v_left['positive'] = fuzz.trimf(v_left.universe, [0.0, self.max_speed-0.1, self.max_speed-0.1])
-		v_left['negative'] = fuzz.trimf(v_left.universe, [-self.max_speed, -self.max_speed, 0.0])
-		v_right['positive'] = fuzz.trimf(v_left.universe, [0.0, self.max_speed, self.max_speed])
-		v_right['negative'] = fuzz.trimf(v_left.universe, [-self.max_speed+0.1, -self.max_speed+0.1, 0.0])
+        v_left['positive'] = fuzz.trimf(v_left.universe, [0.0, self.max_speed - 0.1, self.max_speed - 0.1])
+        v_left['negative'] = fuzz.trimf(v_left.universe, [-self.max_speed, -self.max_speed, 0.0])
+        v_right['positive'] = fuzz.trimf(v_left.universe, [0.0, self.max_speed, self.max_speed])
+        v_right['negative'] = fuzz.trimf(v_left.universe, [-self.max_speed + 0.1, -self.max_speed + 0.1, 0.0])
 
-		rule_l1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] | distance[2]['close'] | distance[3]['close'], v_left['positive'])
-		rule_l2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] | distance[6]['close'] | distance[7]['close'], v_left['negative'])
-		rule_l3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] & distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_left['positive'])
+        rule_l1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] | distance[2]['close'] | distance[3]['close'], v_left['positive'])
+        rule_l2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] | distance[6]['close'] | distance[7]['close'], v_left['negative'])
+        rule_l3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] & distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_left['positive'])
+
+        rule_r1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] | distance[2]['close'] | distance[3]['close'], v_right['negative'])
+        rule_r2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] | distance[6]['close'] | distance[7]['close'], v_right['positive'])
+        rule_r3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] & distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_right['positive'])
+
+        vel_ctrl = ctrl.ControlSystem([rule_l1, rule_l2, rule_l3, rule_r1, rule_r2, rule_r3])
+        self.fuzzy_system = ctrl.ControlSystemSimulation(vel_ctrl)
+
+    def get_vel(self, dist):
+        for i in range(len(dist)):
+            self.fuzzy_system.input['s' + str(i)] = dist[i]
+
+        self.fuzzy_system.compute()
+        return [self.fuzzy_system.output['vl'], self.fuzzy_system.output['vr']]
 
 
-		rule_r1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] | distance[2]['close'] | distance[3]['close'], v_right['negative'])
-		rule_r2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] | distance[6]['close'] | distance[7]['close'], v_right['positive'])
-		rule_r3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] & distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_right['positive'])
+robot = Robot()
+a = avoid_obstacles()
+a.init_fuzzy()
+ultrassonic = robot.read_ultrassonic_sensors()[0:8]
+print("Ultrassonic: ", ultrassonic)
+print("vel: ", a.get_vel(ultrassonic))
 
-		vel_ctrl = ctrl.ControlSystem([rule_l1, rule_l2, rule_l3, rule_r1, rule_r2, rule_r3])
-		self.fuzzy_system = ctrl.ControlSystemSimulation(vel_ctrl)
-
-	def get_vel(self, dist):
-		for i in range(len(dist)):
-			self.fuzzy_system.input['s'+str(i)] = dist[i]
-
-		self.fuzzy_system.compute()
-		return [self.fuzzy_system.output['vl'], self.fuzzy_system.output['vr']]
+for x in range(350):
+    ultrassonic = robot.read_ultrassonic_sensors()[0:8]
+    vel = a.get_vel(ultrassonic)
+    print("vel: ", vel)
+    robot.set_left_velocity(vel[0])  # rad/s
+    robot.set_right_velocity(vel[1])
+    time.sleep(0.2)
--- vrep-robot-master/examples/reading_sensors.py
+++ vrep-robot-master/examples/reading_sensors.py
@@ -1,9 +1,11 @@
-import sys, time, cv2
+import sys
+import time
 from matplotlib import pyplot as plt
 
 sys.path.insert(0, '../src')
 from robot import Robot
-from utils import *
+# from utils import *
+
 
 def display_image(image):
     """
@@ -11,21 +13,22 @@ def display_image(image):
         Args:
             image: The BGR image numpy array. See src/utils.py.
     """
-    plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
+    plt.imshow(image, cmap='gray', interpolation='bicubic')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
 
+
 robot = Robot()
 
-#Reading ultrassonic sensors
+# Reading ultrassonic sensors
 ultrassonic = robot.read_ultrassonic_sensors()
 print("Ultrassonic: ", ultrassonic)
 
-#Reading laser sensor
+# Reading laser sensor
 laser = robot.read_laser()
 print("Laser: ", laser)
 
-#Reading camera
-resolution, raw_img = robot.read_vision_sensor()
-img = vrep2array(raw_img, resolution)
-display_image(img)
+# Reading camera
+# resolution, raw_img = robot.read_vision_sensor()
+# img = vrep2array(raw_img, resolution)
+# display_image(img)
