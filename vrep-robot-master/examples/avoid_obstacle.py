9ac0f54c621fc86c8994bf11305a50855e44d952
Raissa Correia <raisssa.correia@gmail.com>
2019-06-16 15:23:55 -0300

    tentando sensores traseiros

--- vrep-robot-master/examples/avoid_obstacle.py
+++ vrep-robot-master/examples/avoid_obstacle.py
@@ -36,13 +36,13 @@ class avoid_obstacles():
                 distance[i]['away'] = fuzz.trapmf(
                     distance[i].universe, [0.45, 0.9, 5.0, 5.0])
             #comportamento mais de mudanca em cima da hora
-            if(behavior == "mto agressivo"):
+            if(behavior == "agressivo2"):
                 distance[i]['close'] = fuzz.trapmf(
                     distance[i].universe, [0.0, 0.0, 0.1, 0.25])
                 distance[i]['away'] = fuzz.trapmf(
                     distance[i].universe, [0.1, 0.25, 5.0, 5.0])
             #comportamento mais de mudanca em cima da hora
-            if(behavior == "agressivo"):
+            if(behavior == "agressivo1"):
                 distance[i]['close'] = fuzz.trapmf(
                     distance[i].universe, [0.0, 0.0, 0.13, 0.3])
                 distance[i]['away'] = fuzz.trapmf(
@@ -54,41 +54,38 @@ class avoid_obstacles():
         v_right = ctrl.Consequent(
             np.arange(-self.max_speed, 1.0+self.max_speed, 0.01), 'vr')
 
+        #indo em frente roda esquerda
         v_left['positive'] = fuzz.trimf(
             v_left.universe, [0.0, self.max_speed-0.1, self.max_speed-0.1])
+        #indo para tras roda esquerda
         v_left['negative'] = fuzz.trimf(
             v_left.universe, [-self.max_speed, -self.max_speed, 0.0])
+        #indo em frente roda direita
         v_right['positive'] = fuzz.trimf(
             v_left.universe, [0.0, self.max_speed, self.max_speed])
+        #indo para tras roda esquerda
         v_right['negative'] = fuzz.trimf(
             v_left.universe, [-self.max_speed+0.1, -self.max_speed+0.1, 0.0])
 
+        #resumo das regras originais:
+        #sensores 0-3 proximos implica em virar a direita, com a esquerda indo para frente e a direita para tras
         rule_l1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] |
                             distance[2]['close'] | distance[3]['close'], v_left['positive'])
-        rule_l2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] |
-                            distance[6]['close'] | distance[7]['close'], v_left['negative'])
-        rule_l3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] &
-                            distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_left['positive'])
-        #rule_l4 = ctrl.Rule((distance[0]['close'] | distance[1]['close'] |
-        #                    distance[2]['close'] | distance[3]['close'] ) &
-        #                    (distance[4]['close'] | distance[5]['close'] |
-        #                    distance[6]['close'] | distance[7]['close']) & 
-        #                    (distance[8]['away'] | distance[9]['away'] | distance[10]['away'] | distance[11]['away'] |
-        #                    distance[12]['away'] | distance[13]['away'] | distance[14]['away'] | distance[15]['away']), v_left['negative'])
-
         rule_r1 = ctrl.Rule(distance[0]['close'] | distance[1]['close'] |
                             distance[2]['close'] | distance[3]['close'], v_right['negative'])
+
+        #sensores 4-7 proximos implica em virar a esquerda, com a esquerda indo para tras e a direita para frente
+        rule_l2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] |
+                            distance[6]['close'] | distance[7]['close'], v_left['negative'])
         rule_r2 = ctrl.Rule(distance[4]['close'] | distance[5]['close'] |
                             distance[6]['close'] | distance[7]['close'], v_right['positive'])
+
+        #sensores 0-7 distantes implica em ir em frente ambas as rodas
+        rule_l3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] &
+                            distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_left['positive'])
         rule_r3 = ctrl.Rule(distance[0]['away'] & distance[1]['away'] & distance[2]['away'] & distance[3]['away'] &
                             distance[4]['away'] & distance[5]['away'] & distance[6]['away'] & distance[7]['away'], v_right['positive'])
-        #rule_r4 = ctrl.Rule((distance[0]['close'] | distance[1]['close'] |
-        #                    distance[2]['close'] | distance[3]['close'] ) &
-        #                    (distance[4]['close'] | distance[5]['close'] |
-        #                    distance[6]['close'] | distance[7]['close']) & 
-        #                    (distance[8]['away'] | distance[9]['away'] | distance[10]['away'] | distance[11]['away'] |
-        #                    distance[12]['away'] | distance[13]['away'] | distance[14]['away']| distance[15]['away']), v_right['negative'])
-
+       
         vel_ctrl = ctrl.ControlSystem(
             #[rule_l1, rule_l2, rule_l3, rule_l4, rule_r1, rule_r2, rule_r3, rule_r4]
             [rule_l1, rule_l2, rule_l3, rule_r1, rule_r2, rule_r3]
@@ -107,7 +104,7 @@ robot = Robot()
 Avoid = avoid_obstacles()
 
 behavior_type = input()
-#normal, previnido, agressivo
+#normal(como o ped), previnido, agressivo1, agressivo2
 
 Avoid.init_fuzzy(behavior_type)
 data_position_x = []
@@ -116,9 +113,12 @@ how_long = int(input())
 t_start = time.time()
 
 while(robot.get_connection_status() != -1):
-    dist = robot.read_ultrassonic_sensors()
-    vel = Avoid.get_vel(dist[:8]) # Using only the 8 frontal sensors
-    #vel = Avoid.get_vel(dist[:16])  
+    dist = robot.read_ultrassonic_sensors()[0:11]
+    dist_ultrassonic = dist[0:8]
+    dist_ultrassonic.append(dist[9])
+    #dist = robot.read_ultrassonic_sensors()
+    vel = Avoid.get_vel(dist_ultrassonic)
+    #vel = Avoid.get_vel(dist[:8]) # Using only the 8 frontal sensors    
     position = robot.get_current_position()
     data_position_x.append(position[0])
     data_position_y.append(position[1])    
